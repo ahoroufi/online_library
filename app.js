87c3f8173e8f4d0074e3886a97037f4b749e73f3
Arash Horoufi <6829077+ahoroufi@users.noreply.github.com>
2025-06-27 19:55:06 -0700

    add create/update functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const bookRoutes = require('./routes/books')
 const ejsMate = require('ejs-mate');
 const Book = require('./models/book');
@@ -19,8 +20,12 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/books', bookRoutes);
+
+app.use('/books/', bookRoutes);
 
 app.listen(3000, () => {
     console.log('Serving on port 3000');
--- routes/books.js
+++ routes/books.js
@@ -5,6 +5,36 @@ const Book = require('../models/book');
 router.get('/', async (req, res) => {
     const books = await Book.find({})
     res.render('books/index', { books });
+});
+
+router.post('/', async (req, res) => {
+    const newBook = new Book(req.body.book);
+    await newBook.save();
+    res.redirect('/books');
+});
+
+router.get('/new', (req, res) => {
+    res.render('books/new');
+});
+
+router.get('/:id/edit', async (req, res) => {
+    const {id} = req.params;
+    const book = await Book.findById(id)
+    res.render('books/edit', {book});
+}) 
+
+router.put('/:id', async (req, res) => {
+    const {id} = req.params;
+    const book = await Book.findByIdAndUpdate(id, {...req.body.book});
+    res.redirect(`/books/${book._id}`);
+
 })
+router.get('/:id', async (req, res) => {
+    const {id} = req.params;
+    const book = await Book.findById(id)
+    res.render('books/show', {book});
+});
+
+
 
 module.exports = router;
